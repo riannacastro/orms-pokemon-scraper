dca6f7f84c92e6c8599d845fd9cbcdfce937df64
Rianna Castro <riannaxcastro@gmail.com>
2021-05-03 10:37:24 -1000

    commit tree bf2b816e565afadafc6f5401ab2d8d735023fe94

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(attr_hash = {})
+        attr_hash.each do |key, value|
+            self.send("#{key}=", value) if respond_to?("#{key}=")
+        end
+
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT * 
+        FROM pokemon
+        WHERE id = ?
+        SQL
+        pokemon = db.execute(sql, id).flatten
+        Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    end
+
 end
